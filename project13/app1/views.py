91bb707d4caf7f40309257109c8cc44dc6bd67e2
rajeshkumarojha-dev <rajeshkumarojha.dev@gmail.com>
2024-11-12 18:43:19 +0530

    update

--- project13/app1/views.py
+++ project13/app1/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def calculator(request):
+    num1 = eval(request.GET['num1'])
+    num2 = eval(request.GET['num2'])
+    num3=num1+num2
+    response = render(request,'app1/add.html',context={"num3":num3,"num1":num1,"num2":num2})
+    return response
+
+def home(request):
+    response = render(request,'app1/home.html')
+    return response
+
+
+def product(request):
+    product_data={'apple':[1200,'apple.avif'],
+                  'ball':[1150,'ball.avif'],
+                  'cat':[1800,'cat.jpg'],
+                  'dog':[1350,'dog.avif'],
+                  'lion':[2490,'lion.avif']
+                  }
+    response = render(request,'app1/product.html',context={"product_data":product_data})
+    return response
+
+def filter(request):
+    names=['rajesh','puja','riya','sradha']
+    emp_data=[{'empNo':101,"name":"Rajesh","sal":12000},
+              {'empNo':102,"name":"Puja","sal":20000},
+              {'empNo':103,"name":"sardha","sal":12500},
+              {'empNo':104,"name":"krishna","sal":11000},
+              {'empNo':105,"name":"biswa","sal":21000}]
+    course=['python','sql','mongodb','django',['HTML','CSS','JS']]
+    
+    response = render(request,'app1/filter.html',context={"names":names,"emp_data":emp_data,"course":course})
+    return response
+
+
+    
--- project13/project13/urls.py
+++ project13/project13/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from app1.views import  *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cal/',calculator),
+    path('',home),
+    path("product/",product),
+    path('filter/',filter)
 ]
